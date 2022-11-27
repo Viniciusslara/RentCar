9d083fdad0dfbfb17f198ff5dfdf6dbbc3db2ac4
Gabriel Perino Feitosa <50030104+gabrielfeitosa00@users.noreply.github.com>
2022-11-27 16:29:05 -0300

    fix: fix main class wrong comment

--- RentcarProject/src/rentcarproject/view/TelaLogin.java
+++ RentcarProject/src/rentcarproject/view/TelaLogin.java
@@ -136,9 +136,7 @@ public class TelaLogin extends javax.swing.JFrame {
         
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
