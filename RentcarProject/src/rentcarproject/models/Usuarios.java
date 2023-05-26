66f09436929db66ec16b5532190e6c2e6d22c7bc
Viniciusslara <vinicius.lara2017@hotmail.com>
2023-05-25 23:53:04 -0300

    add: model users

---
+++ RentcarProject/src/rentcarproject/models/Usuarios.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rentcarproject.models;
+
+/**
+ *
+ * @author vinicius
+ */
+public class Usuarios {
+    
+    private int id;
+    private String username;
+    private String password;
+    
+    public Usuarios(){
+        this.id = 0;
+        this.username = "";
+        this.password = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
